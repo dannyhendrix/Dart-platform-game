5f119e879fae5bfcf51d11149a5a81c155fa1e9c
Danny Hendrix <dwmhendrix@gmail.com>
2016-11-02 20:49:13 +0100

    Restructure.

--- platform-game-web/web/platformgame.dart
+++ platform-game-web/web/platformgame.dart
@@ -4,7 +4,7 @@ Platform game example
 @author Danny Hendrix
 **/
 
-import 'package:dart_platform_game/web.dart';
+import 'package:dart_platform_game_web/web.dart';
 
 void main() {
   Dashboard db = new Dashboard();
--- platform-game/lib/game.dart
+++ platform-game/lib/game.dart
@@ -11,23 +11,25 @@ import "dart:math" as Math;
 import 'utils.dart';
 
 part 'game/game.dart';
-part 'game/camera.dart';
+part 'game/gameoutput.dart';
 part 'game/collisionfield.dart';
-part 'game/level.dart';
-part 'game/leveltile.dart';
-part 'game/renderobject.dart';
+
+part 'game/level/level.dart';
+part 'game/level/leveltile.dart';
+
+part 'game/renderobject/renderobject.dart';
 part 'game/renderobject/gameobject.dart';
 part 'game/renderobject/player.dart';
 part 'game/renderobject/door.dart';
 part 'game/renderobject/flag.dart';
 part 'game/renderobject/interactiveobject.dart';
 part 'game/renderobject/levelobject.dart';
-part 'game/sprite.dart';
-part 'game/animationframes.dart';
-part 'game/gameoutput.dart';
 
 part 'game/render/render.dart';
 part 'game/render/renderlayer.dart';
 part 'game/render/imagetext.dart';
+part 'game/render/sprite.dart';
+part 'game/render/animationframes.dart';
+part 'game/render/camera.dart';
 
 part 'game/resourcemanager/resourcemanager.dart';
